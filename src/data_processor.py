d911b7cf0b974a30d94be940ed97a997e8cbcaa7
Valentin Maksimchuk <maxvale.mail@gmail.com>
2021-06-24 19:03:39 +0300

    Added data process to arrow columnar format

--- src/data_processor.py
+++ src/data_processor.py
@@ -3,14 +3,72 @@ import json
 
 JSON_FILE = '..\\data\\test.json'
 
+JSON_SCHEMA = pa.schema([('timestamp', pa.string()),
+                         ('id',        pa.string()),
+                         ('data',      pa.string())])
+
+
+class ArrowData:
+    def __init__(self, schema):
+        self.schema = schema
+        self.buffer = dict()
+        self.flush_buffer()
+
+    def add_data(self, name, data):
+        for key in self.schema.names:
+            print(key)
+            if key == name:
+                self.buffer[key].append(data[key])
+
+    def flush_buffer(self):
+        self.buffer = dict()
+        for key in self.schema.names:
+            self.buffer[key] = list()
+
+    def get_buffer(self):
+        return self.buffer
+
 
 class ArrowStream:
 
-    def __init__(self):
-        self.buffer = list()
+    def __init__(self, schema):
+        self.event_count = 0
+        self.arrow_data = ArrowData(schema)
+        self.sink = pa.BufferOutputStream()
+        self.writer = pa.ipc.new_stream(self.sink, JSON_SCHEMA)
 
-    def json_converter(self, data):
-        pass
+    def process_segment(self, segment, event_count):
+        reader = pa.BufferReader(segment)
+        step = reader.size() // event_count
+        start = reader.tell()
+        while start < reader.size():
+            data_fragment = reader.read(step)
+            start = reader.tell()
+            print('Position: ' + str(start))
+            print('Data fragment: ' + str(data_fragment))
+            self._process_fragment(data_fragment)
+
+    def create_batch(self):
+        data = list()
+        for key in self.arrow_data.schema.names:
+            data.append(pa.array(self.arrow_data.get_buffer()[key]))
+        batch = pa.record_batch(data, names=self.arrow_data.schema)
+        return batch
+
+    def write_stream(self):
+        batch = self.create_batch()
+        self.writer.write_batch(batch)
+
+    def _process_fragment(self, fragment):
+        fragment_data = json.loads(fragment)
+        for key in fragment_data:
+            self.arrow_data.add_data(key, fragment_data)
+
+    def get_stream(self):
+        return self.sink
+
+    def get_data(self):
+        return self.arrow_data
 
 
 def main():
@@ -18,11 +76,25 @@ def main():
         json_obj = json.load(json_file)
     print(json_obj)
     str_ = json.dumps(json_obj)
-    str_ = ' '.join(format(ord(letter), 'b') for letter in str_)
 
-    json_obj = ''.join(chr(int(x, 2)) for x in str_.split())
-    data = json.loads(json_obj)
-    print(data)
+    print(str_)
+    str_ = bytes(str_, 'utf-8')
+    print(str_)
+    sink = pa.BufferOutputStream()
+    sink.write(str_)
+    sink.write(str_)
+
+    buf = sink.getvalue()
+    ar = ArrowStream(JSON_SCHEMA)
+    ar.process_segment(buf, 2)
+
+    print('/////')
+    ar.write_stream()
+    stream = ar.get_stream()
+    buf = stream.getvalue()
+    reader = pa.ipc.open_stream(buf)
+    print(reader.read_pandas())
+
 
 
 if __name__ == '__main__':
--- src/pravega_reader.py
+++ src/pravega_reader.py
@@ -1,5 +1,10 @@
+import json
+
 import pravega_client as pc
 import pyarrow as pa
+
+
+JSON_FILE = '..\\data\\test.json'
 
 
 class PravegaReader:
@@ -8,26 +13,43 @@ class PravegaReader:
         self.stream = stream
         self.name = name
         self.readers = dict()
-        self.buffer = list()
+        self.buffer = pa.BufferOutputStream()
+        self.event_count = 0
 
         manager = pc.StreamManager(host)
-        self.reader_group = manager.create_reader_group(self.name, self.scope, self.stream)
+        self.reader_group = manager.create_reader_group(self.name,
+                                                        self.scope,
+                                                        self.stream)
 
     def add_reader(self, name):
         self.readers[name] = self.reader_group.create_reader(name)
         
-    async def read_event_async(self, name):
+    async def read_segment_async(self, name):
         reader = self.readers[name]
         segment_slice = await reader.get_segment_slice_async()
         print(segment_slice)
         for item in segment_slice:
             print(item.data())
-            self.buffer.append(item.data())
+            self.buffer.write(item.data())
+            self.event_count += 1
         reader.release_segment(segment_slice)
         reader.reader_offline()
         del self.readers[name]
 
     def get_buffer(self):
-        result = self.buffer
-        self.buffer = list()
-        return result
+        return self.buffer.getvalue()
+
+    def flush_buffer(self):
+        self.buffer.flush()
+        self.event_count = 0
+
+    def test_write_data(self):
+        with open(JSON_FILE) as json_file:
+            json_obj = json.load(json_file)
+        print(json_obj)
+        data_string = json.dumps(json_obj)
+        print(data_string)
+        data_string = bytes(data_string, 'utf-8')
+        data = data_string + data_string
+        self.buffer.write(data)
+        self.event_count = 2
