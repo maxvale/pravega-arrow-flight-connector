c861a41a8d7831294c93aa1740ed7691a5873f65
Valentin Maksimchuk <maxvale.mail@gmail.com>
2021-07-01 00:32:31 +0300

    Add writer for tests

--- src/pravega_writer.py
+++ src/pravega_writer.py
@@ -1,1 +1,41 @@
+import json
+import random
+import time
+from datetime import datetime
+
+import yaml
+
 import pravega_client as pc
+
+CONFIG_FILE = 'config.yaml'
+
+JSON_FILE = '..\\data\\test1.json'
+
+
+def write_event(writer):
+    with open(JSON_FILE) as json_file:
+        json_obj = json.load(json_file)
+    json_obj['timestamp'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    json_obj['data'] = random.randint(0, 128)
+    str_ = json.dumps(json_obj)
+    str_ = bytes(str_, 'utf-8')
+    print(str_)
+    writer.write_event_bytes(str_)
+
+
+def main():
+
+    with open(CONFIG_FILE) as yaml_file:
+        config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    manager = pc.StreamManager(config['pravega'], False, False)
+    manager.create_scope('scope')
+    manager.create_stream('scope', 'stream', 1)
+    writer = manager.create_writer('scope', 'stream')
+
+    while True:
+        write_event(writer)
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
